3ab458dd6abdd952ce9e5cb9a100f3c7e5dc6a34
Keremalp Demiröz <116038267+KeremalpDemiroz@users.noreply.github.com>
2025-06-24 11:52:48 +0300

    Update ft_itoa.c

--- Libft/ft_itoa.c
+++ Libft/ft_itoa.c
@@ -1,25 +1,53 @@
+
+
+#include "libft.h"
 #include <stdio.h>
-int	ft_itoa(int n)
+
+int	ft_digits(int n)
 {
 	int	digits;
-	int sign;
-	unsigned int i;
 
-	i = n;
 	digits = 0;
-	sign = 1;
+	if (n == 0)
+		return (1);
 	if (n < 0)
-		sign = -1;
-	while (n > 0)
+		digits = 1;
+	while (n != 0)
 	{
 		n = n / 10;
 		digits++;
 	}
 	return (digits);
-	
-	
+}
+
+char	*ft_itoa(int n)
+{
+	char	*result;
+	int		digits;
+	int		temp;
+
+	temp = n;
+	digits = ft_digits(n);
+	result = malloc(sizeof(char) * digits + 1);
+	if (!result)
+		return (NULL);
+	result[digits--] = '\0';
+	if (temp == 0)
+		return ("0");
+	if (temp  == -2147483648)
+		return ("-2147483648");
+	if (temp < 0)
+		temp = -temp;
+	while (digits >= 0)
+	{
+		result[digits--] = (temp % 10) + 48;
+		temp /= 10;
+	}
+	if (n < 0)
+	result[0] = '-';
+	return (result);
 }
 int main()
 {
-	printf("%d\n", ft_itoa(1234567890123456));
+	printf("%s\n", ft_itoa());
 }
